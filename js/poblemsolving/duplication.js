212cb66d23b8f778e2a2700195e89b49da62d0a1
AlyNefel <ali.nefel2020@gmail.com>
2025-08-31 18:05:09 +0100

    Finish store

---
+++ js/poblemsolving/duplication.js
@@ -0,0 +1,65 @@
+/*
+input : var array = [3,5,4,5,1,2,98,1,3]
+output : var result = [3,5,4,1,2,98]
+
+*/ 
+
+function removeDuplication(ali){
+    //todo
+    // prepare an empty array to hold the result
+    var result = []
+    for (let i = 0; i < ali.length; i++) {
+        console.log("setp : " +i)
+        console.log(ali[i])
+        console.log(result.includes(ali[i]))
+        if(!result.includes(ali[i])){
+            result.push(ali[i])
+        }
+        console.log(result)
+    }
+    // console.log(i)
+    return result
+}
+ console.log(removeDuplication([5,8,5,7,56,54]))
+//console.log(result)
+/*
+var array = [3,5,4,5,1,2,98,1,3]
+array.push(70) =>[3,5,4,5,1,2,98,1,3,70]
+array.includes(225)  => true || false *
+*/
+// var array = [3,5,4,5,1,2,98,1,3,5] 
+//console.log(removeDuplication(array))
+// function removeDuplication1(array){
+// return [...new Set(array)]
+// }
+// var array = [3,5,4,5,1,2,98,1,3,5] 
+// var testSet = [new Set(array)]
+/*
+Cls Person {
+name;
+age;
+city;
+}
+person1 = new Person("Yassin",25,Tunis)
+*/ 
+// console.log(testSet)
+
+// var array1 = [...array]
+// console.log(array1)
+// array1.push(9999)
+// console.log(array1)
+// console.log("this is the first array ",array)
+
+// var name="Ali"
+// // in java : public String name = "Ali"
+// name = 25
+// console.log(name)
+// const array = [1,2,3]
+// array.push(4)
+// console.log(array)
+// array="Test"
+let arr1=[1,2,5]
+let arr2=[99,75,6]
+
+let array3=[...arr1,...arr2]
+console.log(array3)
--- js/shop/script.js
+++ js/shop/script.js
@@ -1,24 +1,84 @@
-function increaseQuantity(){
-    // select the input (number)
-    var input =document.querySelector("#quantity")
-    var unitPrice = document.querySelector("#unit-price")
-     var totalPrice = document.querySelector("#total-price")
-    // console.log(typeof(input.value) )
-    if( Number(input.value)<=9){
+// function increaseQuantity(){
+//     // select the input (number)
+//     var input =document.querySelector("#quantity")
+//     var unitPrice = document.querySelector("#unit-price")
+//      var totalPrice = document.querySelector("#total-price")
+//     // console.log(typeof(input.value) )
+//     if( Number(input.value)<=9){
+
+//         input.value= Number(input.value)+1
+//     }
+//     // change the price span 
+//     var newTotalPrice=Number(unitPrice.value)*Number(input.value)
+//     totalPrice.textContent=newTotalPrice
+
+// }
+
+// function decreaseQuantity(){
+//    var input =document.querySelector("#quantity")
+//    var value =Number(input.value)
+//    // condition to stop minus when we reash 1
+//    if(value>1){
+//     input.value=value-1
+//    }
+// }
+
+function increaseQuantity(btn){
+console.log(btn)
+// select the correct input 
+//btn.nextElementSibling
+console.log(btn.previousElementSibling)
+var input = btn.previousElementSibling
+if( Number(input.value)<=9){
 
         input.value= Number(input.value)+1
     }
-    // change the price span 
-    var newTotalPrice=Number(unitPrice.value)*Number(input.value)
-    totalPrice.textContent=newTotalPrice
 
 }
 
-function decreaseQuantity(){
-   var input =document.querySelector("#quantity")
-   var value =Number(input.value)
+function decreaseQuantity(btn){
+ var input =btn.nextElementSibling
+  var value =Number(input.value)
    // condition to stop minus when we reash 1
-   if(value>1){
-    input.value=value-1
-   }
+   if(value>1){  
+       input.value=value-1
+    }
+}
+
+// hover image
+function hovered(ali){
+console.log(ali)
+// i wil lincrease the width of the image (style)
+ali.style.width="250px"
+ali.style.border="2px solid red"
+}
+
+//unhover image
+function unhovered(image){
+    image.style.width="200px"
+image.style.border ="0px solid white"
+}
+
+//show reveiw div
+function showReview(){
+var box = document.querySelector(".review")
+console.log(box)
+box.style.display="block"
+}
+
+// show comment 
+function showComment(container,content){
+    // target element || container => fil p element
+    var para = document.querySelector(container)
+    //content bech njibou mel input 
+    var text = document.querySelector(content)
+
+    para.textContent = text.value
+}
+
+function submitReview(comment,input){
+
+var text =document.querySelector(comment)
+document.querySelector(input).value=''
+text.style.display="none"
 }
