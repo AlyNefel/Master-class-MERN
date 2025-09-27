c9bdbd5d6e7073c1795f1b48e362e825e990b08d
AlyNefel <ali.nefel2020@gmail.com>
2025-09-27 16:53:50 +0100

    useEffect

--- React/useEffect/src/App.jsx
+++ React/useEffect/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
@@ -8,6 +8,9 @@ function App() {
   
 const [lanches,setLanches]=useState([])
 const [lanchesAxios,setLanchesAxios]=useState([])
+const [searchWord,setSerachWord]=useState("")
+const [filtredLanches,setFiltredLanches]=useState([])
+const [count,setCount]=useState(0)
 
 // native fetch (before axios)
 const fetchLanches=()=>{
@@ -22,16 +25,45 @@ const fetchLanches=()=>{
 }
 
 // fetch lanches with axios 
-const fetchLanchesAxios =()=>{
-  axios.get("https://api.spacexdata.com/v3/launches")
-  .then(response=>setLanchesAxios(response.data.slice(0,6)))
+const fetchLanchesAxios =async()=>{
+  await axios.get("https://api.spacexdata.com/v3/launches")
+  .then(response=>{
+    
+    const first6Lanches=response.data.slice(0,6)
+    const filtredLanches1=response.data.filter(lanch=>lanch.mission_name.includes(searchWord)) 
+  // set axios lanches
+  console.log(filtredLanches1)
+  setLanchesAxios(filtredLanches1)
+  setFiltredLanches(filtredLanches1)
+  }
+  )
 }
 
+// we will get the llanches using useEffect withouth button click 
+useEffect(()=>{
+  fetchLanchesAxios()
+  
+},[searchWord])
+//skddhbfkhsdhk
 // const person ={name:"abdelbasset"}  // json format => {"name":"abdelbasset"}
   return (
     <div className="container mt-5">
+      <div className="d-flex justify-content-between mb-4">
+<input
+ type="text" 
+ className="form-control"
+ value={searchWord}
+ onChange={(e)=>setSerachWord(e.target.value)}
+ />
+ {searchWord.length && filtredLanches.map((lanch,index)=>{
+  return(<span class="badge rounded-pill bg-primary">{lanch.mission_name}</span>)
+ })}
+ 
+ 
+      </div>
+      <h3 className="mt-4">for the word <strong>{searchWord}</strong> you have <strong>{filtredLanches.length}</strong> result</h3>
       <h1>Get 6 lanches with fetch</h1>
-           <button className="btn btn-primary" onClick={fetchLanches}>with fetch</button>
+           {/* <button className="btn btn-primary" onClick={fetchLanches}>with fetch</button> */}
 
       <div className="row">
 {lanches.map((lanch,index)=>{
@@ -56,7 +88,7 @@ return(<div key={index} className="col-md-4 mb-4">
 })}
       </div>
 <h1>Get 6 lanches with AXIOS:</h1>
-                <button className="btn btn-primary" onClick={fetchLanchesAxios}>with Axios</button>
+                {/* <button className="btn btn-primary" onClick={fetchLanchesAxios}>with Axios</button> */}
 <div className="row">                
 {lanchesAxios.map((lanch,index)=>{
 return(<div key={index} className="col-md-4 mb-4">
